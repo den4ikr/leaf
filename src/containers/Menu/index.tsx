e726702e7dc2058b0a9395fef058c252ace29371
Danik Roman <danirom0401@gmail.com>
2023-12-05 14:15:01 +0200

    Menu: add back navigation on backArrow click

--- src/containers/Menu/index.tsx
+++ src/containers/Menu/index.tsx
@@ -3,12 +3,21 @@ import classes from "./styles.module.css";
 import ArrowBackIcon from "../../icons/ArrowBackIcon";
 import products from "../../constants/products";
 import ProductCard from "../../components/ProductCard";
+import { useNavigate } from "react-router-dom";
 
 const MenuPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={classes.root}>
       <div>
-        <ArrowBackIcon />
+        <div onClick={handleBackClick} style={{ cursor: "pointer" }}>
+          <ArrowBackIcon />
+        </div>
         <h3 className={classes.title}>Продукція</h3>
       </div>
       <div className={classes.row}>
